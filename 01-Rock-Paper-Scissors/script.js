a5b6e3c6ef2417454a8b10c1f6aee7e3cee0e1db
jasmeettt <jasmeetsalhotrawork@gmail.com>
2026-01-04 17:35:44 +0530

    Fix animations and hand orientation

--- 01-Rock-Paper-Scissors/script.js
+++ 01-Rock-Paper-Scissors/script.js
@@ -4,32 +4,65 @@ let cpuResult = document.querySelector(".cpu-result img");
 let result = document.querySelector(".result");
 let optionImages = document.querySelectorAll(".option-image");
 
+let cpuImages = [
+  "./Images/rock.png",
+  "./Images/paper.png",
+  "./Images/scissors.png",
+];
+
+const choices = ["R", "P", "S"];
+
 optionImages.forEach((image, index) => {
   image.addEventListener("click", (e) => {
     image.classList.add("active");
+
+    userResult.src = cpuResult.src = "./Images/rock.png"
+    result.textContent = "Playing..."
+
 
     //checking if index matches and then removing the active class.
     optionImages.forEach((image2, index2) => {
       index !== index2 && image2.classList.remove("active");
     });
 
-    //getting the image src
+    container.classList.add("start")
+
+    //setting a timeout for the process to take its time
+    let time = setTimeout(()=>{
+
+    container.classList.remove("start")
+
+      //getting the image src
     //user image
     let imgSrc = image.querySelector("img").src;
     userResult.src = imgSrc;
 
     //cpu image
     let randomNumber = Math.floor(Math.random() * 3);
-    let cpuImages = [
-      "./Images/rock.png",
-      "./Images/paper.png",
-      "./Images/scissors.png",
-    ];
     cpuResult.src = cpuImages[randomNumber];
 
     //getting the values from the user and cpu as R,P,S
-    let userValue = ["R", "P", "S"][index];
-    let cpuValue = ["R", "P", "S"][randomNumber];
-    console.table(userValue,cpuValue)
+    let userValue = choices[index];
+    let cpuValue = choices[randomNumber];
+
+    //objext with all possible out comes
+    let outcomes = {
+      RR: "Draw",
+      RP: "Cpu",
+      RS: "User",
+      PP: "Draw",
+      PS: "Cpu",
+      PR: "User",
+      SS: "Draw",
+      SR: "Cpu",
+      SP: "User",
+    };
+
+    //looking up the outcomes based on the cpu and user value
+    let outcomeValue = outcomes[userValue + cpuValue];
+
+    //display result
+    result.textContent = userValue === cpuValue ? "Match Draw" : `${outcomeValue} Won !!`
+    },2500)
   });
 });
